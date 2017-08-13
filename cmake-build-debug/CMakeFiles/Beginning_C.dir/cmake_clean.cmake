b764bcf999553a9ca81c3ac0a1227ceb11ac99c4
Kell-Wang <2416451355@qqcom>
2017-08-13 21:29:52 +0800

    8-13-21:30-first Commit

---
+++ cmake-build-debug/CMakeFiles/Beginning_C.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Beginning_C.dir/main.c.obj"
+  "Beginning_C.pdb"
+  "Beginning_C.exe"
+  "Beginning_C.exe.manifest"
+  "libBeginning_C.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Beginning_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
